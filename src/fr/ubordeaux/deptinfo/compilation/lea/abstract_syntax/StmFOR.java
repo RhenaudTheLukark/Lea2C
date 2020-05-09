706488e7cec87d0617d4f679b3f60d524b8c8a88
picachoc <loic.lachiver@gmail.com>
2020-05-09 18:17:33 +0200

    working for + simplified progr-6.lea (with backup)

--- src/fr/ubordeaux/deptinfo/compilation/lea/abstract_syntax/StmFOR.java
+++ src/fr/ubordeaux/deptinfo/compilation/lea/abstract_syntax/StmFOR.java
@@ -11,6 +11,40 @@ public class StmFOR extends StmTernary {
 		super(stm1, stm2, stm3, line, column);
 		this.expr = expr;
 	}
+
+	@Override
+	public String generateCode() throws CodeException {
+		String result = "";
+		result += super.generateCode();
+
+		String var_test = "_var_for_test__" + this.getId();
+		String label_for_test = "_for_label_test_" + this.getId();
+		String label_for_code = "_for_label_code_" + this.getId(); 
+		String label_for_end = "_for_label_end_" + this.getId(); 
+		
+		result += tab() + getFirst().generateCode() + NL; //initialization
+		result += tab() + "int " + var_test + ";" + NL;
+		
+		result += tab() + label_for_test + ":{}" + NL;
+		
+		result += tab() + var_test + " = " + expr.generateCode() + ";" + NL;
+		
+		result += tab() + "if (" + var_test + ")" + NL; //stopping condition
+		incIndent();
+			result += tab() + "goto " + label_for_code + ";" + NL;
+		decIndent();
+		result += tab() + "goto " + label_for_end + ";" + NL;
+		result += tab() + label_for_code + ":{" + NL;
+		incIndent();
+			result += getThird().generateCode();
+			result += getSecond().generateCode(); //increment
+			result += tab() + "goto " + label_for_test + ";" + NL;
+		decIndent();
+		result += tab() + "}" + NL;
+		result += tab() + label_for_end + ":{}" + NL;
+		
+		return result;
+	}
 	
 	@Override
 	public void checkType() throws TypeException {
@@ -22,19 +56,6 @@ public class StmFOR extends StmTernary {
 		if (this.getThird() != null)
 			this.getThird().checkType();
 		expr.getType().assertType(this, TypeCode.BOOLEAN);
-	}
-
-	@Override
-	public String generateCode() throws CodeException {
-		String result = "";
-		result += super.generateCode();
-		// TODO
-		result += tab() + "_FOR_" + this.getId() + ":" + NL;
-		this.incIndent();
-		result += tab() + "// Code FOR ici..." + NL;
-		result += tab() + "printf(\"--- Manque FOR...\\n\");" + NL;
-		this.decIndent();
-		return result;
 	}
 
 	@Override
