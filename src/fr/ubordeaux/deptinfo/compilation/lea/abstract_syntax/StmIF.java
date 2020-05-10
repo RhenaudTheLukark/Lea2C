894539ecb16714675c9ebf4f307e48072d5b8aa6
romainpc <romainpc.lechat@laposte.net>
2020-05-10 20:12:58 +0200

    Implement ELSE in StmIF

--- src/fr/ubordeaux/deptinfo/compilation/lea/abstract_syntax/StmIF.java
+++ src/fr/ubordeaux/deptinfo/compilation/lea/abstract_syntax/StmIF.java
@@ -17,34 +17,41 @@ public class StmIF extends StmBinary {
 		String result = "";
 		result += super.generateCode();
 
-		if (this.getRight()!=null) {
-			result += tab() + "// Ooops, ELSE pas encore supporté, seul le THEN est généré ici" + NL;
-		}
-
 		String var = "_if_test__" + this.getId();
 		String label_then = "_if_label_then__" + this.getId();
 		String label_fin = "_if_label_fin__" + this.getId();
+		String label_else = "_if_label_else__" + this.getId();
 		result += tab() + "int " + var + " = " + test.generateCode() + ";" + NL;
 		result += tab() + "if (" + var + ")" + NL;
 		incIndent();
-			result += tab() + "goto " + label_then + ";" + NL;
+		result += tab() + "goto " + label_then + ";" + NL;
 		decIndent();
+
+		if (this.getRight() != null) {
+			result += tab() + label_else + ":{" + NL;
+			incIndent();
+			result += getRight().generateCode();
+			decIndent();
+			result += tab() + "}" + NL;
+		}
+
 		result += tab() + "goto " + label_fin + ";" + NL;
 		result += tab() + label_then + ":{" + NL;
 		incIndent();
-			result += getLeft().generateCode();
+		result += getLeft().generateCode();
 		decIndent();
 		result += tab() + "}" + NL;
 		result += tab() + label_fin + ":{}" + NL;
 		return result;
+
 	}
 
 	@Override
 	public void checkType() throws TypeException {
 		test.checkType();
-		if (this.getLeft()!=null)
+		if (this.getLeft() != null)
 			getLeft().checkType();
-		if (this.getRight()!=null)
+		if (this.getRight() != null)
 			getRight().checkType();
 		test.getType().assertType(this, TypeCode.BOOLEAN);
 	}
